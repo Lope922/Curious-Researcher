f5c09364600c36a23acb15e099fec015bddc1592
jessesomething <jessespalding@gmail.com>
2016-03-13 19:28:04 -0500

    Prints out species occurences's country of observations based on gbif taxon key

--- Main.py
+++ Main.py
@@ -3,7 +3,21 @@
 import pygbif as bgif
 from pygbif import registry
 from pygbif import species
+from pygbif import occurrences
+from bs4 import BeautifulSoup
+import requests
+import json
 
-suggest = species.name_suggest(q="Blue bird")
+# suggest = species.name_suggest(q="blue whale")
+#
+# print(suggest)
 
-print(suggest)
+number = occurrences.search(taxonKey=5959227, limit=250)
+
+occurs = number
+
+for o in occurs['results']:
+    country = o['country']
+    print(country)
+
+print(number)
