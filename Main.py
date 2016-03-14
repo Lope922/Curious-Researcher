e5b240cd7c1677326bbdea2bcd32c14ca85eb507
jessesomething <jessespalding@gmail.com>
2016-03-13 20:48:21 -0500

    Searches out counts of animals Currently not showing accurate results (i.e. Searching tiger comes up with lots of different insects)

--- Main.py
+++ Main.py
@@ -8,16 +8,40 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-# suggest = species.name_suggest(q="blue whale")
-#
+suggest = species.name_suggest(q="white tiger", limit=50)
+
+suggest_data = suggest['data']
+
+print(suggest['data'])
+
+data = suggest_data
+
+for o in data:
+    key = o['key']
+    name = o['canonicalName']
+    # print(key)
+    # print(name)
+    occurs = occurrences.count(taxonKey=key)
+    print('Name: ' + name + '\nCount: ' + str(occurs))
+
+# print(suggest['data'])
+
 # print(suggest)
-
-number = occurrences.search(taxonKey=5959227, limit=250)
-
-occurs = number
-
-for o in occurs['results']:
-    country = o['country']
-    print(country)
-
-print(number)
+#
+# number = occurrences.search(taxonKey=5959227, limit=250)
+# occurs = number
+#
+# country_count = dict
+#
+#
+#
+# for o in occurs['results']:
+#     country = o['country']
+#
+#     # Reads image of occurrence
+#     # Could be used for the GUI images
+#     # image = o['media']['identifier']
+#
+#     print(country)
+#
+# print(number)
