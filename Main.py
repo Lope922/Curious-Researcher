fb8c945a4e0f901a2bf5f913e7995f335f8390cf
jessesomething <jessespalding@gmail.com>
2016-03-20 23:48:17 -0500

    Improved results with try/catch again

--- Main.py
+++ Main.py
@@ -21,46 +21,49 @@ print(suggest_data)
 data = suggest_data
 
 for o in data:
-    # print(o)
     key = o['key']
     occurs = occurrences.count(taxonKey=key)
-    try :
-        canon_name = o['canonicalName']
-    except:
-        continue
-    names = o['vernacularNames']
-    descriptions = o['descriptions']
     if occurs > 0:
-        print('Scientific name: ' + canon_name + '\n')
-        print('Vernacular names: ')
-        for name in names:
-            # Used if all languages want to be included
-            # print(name['vernacularName'])
-            language = (name['language'])
-            # Can be changed if user wants to select a specific language
-            if language == 'eng':
-                print(name['vernacularName'])
-        # print(wikipedia.search(canon_name))
+        print()
         try:
-            # pulls the wiki page based on canonical name in url (usually works)
-            desc = wikipedia.page(canon_name, auto_suggest=True)
-            # alternative wiki page including all sections
-            page = wikipedia.WikipediaPage(canon_name)
-            # pulls the summary page for the species
-            summary = wikipedia.summary(canon_name, sentences=5)
-            # experimental for section pages
-            # sections = page.sections
-            # for section in sections:
-            #     print(section)
-            # github push error, delete this
-            print(page.summary)
-            # print(desc.content)
+            canon_name = o['canonicalName']
         except:
-            print("No description found")
+            continue
+        names = o['vernacularNames']
+        descriptions = o['descriptions']
+        try:
+            print('Scientific name: ' + canon_name)
+            print('Vernacular names: ')
+            for name in names:
+                # Used if all languages want to be included
+                # print(name['vernacularName'])
+                language = (name['language'])
+                # Can be changed if user wants to select a specific language
+                if language == 'eng':
+                    print(name['vernacularName'])
+            # print(wikipedia.search(canon_name))
+            try:
+                # pulls the wiki page based on canonical name in url (usually works)
+                desc = wikipedia.page(canon_name, auto_suggest=True)
+                # alternative wiki page including all sections
+                page = wikipedia.WikipediaPage(canon_name)
+                # pulls the summary page for the species
+                summary = wikipedia.summary(canon_name, sentences=5)
+                # experimental for section pages
+                # sections = page.sections
+                # for section in sections:
+                #     print(section)
+                # github push error, delete this
+                print(page.summary)
+                # print(desc.content)
+            except:
+                print("No description found")
 
-        print('GBIF Key: ' + str(key))
-        print('GBIF Species Page: ' + 'http://www.gbif.org/species/' + str(key))
-        print('Count: ' + str(occurs) + '\n')
+            print('GBIF Key: ' + str(key))
+            print('GBIF Species Page: ' + 'http://www.gbif.org/species/' + str(key))
+            print('Count: ' + str(occurs) + '\n')
+        except:
+            continue
 
 # occurs = occurrences.search()
 
