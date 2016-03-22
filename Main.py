e3881aa2ade77a7deaadd9e6171164aeb0084b32
Jesse <jessespalding@gmail.com>
2016-03-22 01:50:57 -0500

    Added return functionality to results Return will provide a dictionary of results with scientific name as key, and values of vernacular name and two sentence description from wikipedia

--- Main.py
+++ Main.py
@@ -7,53 +7,74 @@ import requests
 import json
 import wikipedia
 
-def search_species(entry):
 
+def search_species(entry):
+    # To be used when calling the method
     # name_input = entry
 
+    # User input for testing purposes
     name_input = input("Enter a common species name: ")
 
-    suggest = species.name_suggest(q=name_input, rank='SPECIES', limit=250)
+    results = {}
 
+    # Uses the pygbif method to find results
+    suggest = species.name_suggest(q=name_input, rank='SPECIES', limit=25)
+
+    # Pulls the results from the dataset in json format
     suggest_data = suggest['data']['results']
 
-    print(suggest_data)
+    # print(suggest_data)
 
+    # Formats the data for pythonic purposes
     data = suggest_data
 
+    # Reads data for each result
     for o in data:
+        # Finds the gbif key
         key = o['key']
+        # Uses pygbif to find number of occurrences of species key
         occurs = occurrences.count(taxonKey=key)
+        # Searches occurrence data for the species key
         occur_search = occurrences.search(taxonKey=key)
         # print('occur search: ' + str(occur_search))
-
 
         # for country in countries:
         #     print(country)
         # print(occur_search)
+
+        # Runs if species has occurred more than zero times
         if occurs > 0:
             try:
+                # Tries to retrieve scientific name
                 canon_name = o['canonicalName']
             except:
                 continue
+            # Vernacular name init
+            vern_name = ''
+            # Variable for list of vernacular names
             names = o['vernacularNames']
-            descriptions = o['descriptions']
+            # Summary init
+            summary = ''
             try:
                 # If match found
                 print('Scientific name: ' + canon_name)
                 print('Vernacular names: ')
                 match_found = False
+                # Reads from results in matched name
                 for name in names:
-                    vern_name_match = name['vernacularName']
+                    # Variable for vernacular name
+                    vern_name = name['vernacularName']
                     # Used if all languages want to be included
                     # print(name['vernacularName'])
                     language = (name['language'])
                     # Can be changed if user wants to select a specific language
                     if language == 'eng':
-                        if name_input in vern_name_match and match_found is False:
+                        # Checks if vernacular name is matched with search input
+                        if name_input in vern_name and match_found is False:
                             match_found = True
-                            print(vern_name_match)
-                if match_found == False:
+                            print(vern_name)
+                # If no exact match is found it reads the first vernacular name
+                if match_found is False:
                     name_store = []
                     for name in names:
                         vern_name = name['vernacularName']
@@ -62,11 +83,13 @@ def search_species(entry):
                             name_store.append(vern_name)
                             if language == 'eng':
                                 print(vern_name)
+                # Adds scientific and vernacular name to results dictionary
+                results.setdefault(canon_name, []).append(vern_name)
 
                 # print(wikipedia.search(canon_name))
                 try:
                     # pulls the wiki page based on canonical name in url (usually works)
-                    desc = wikipedia.page(canon_name, auto_suggest=True)
+                    # desc = wikipedia.page(canon_name, auto_suggest=True)
                     # alternative wiki page including all sections
                     # page = wikipedia.WikipediaPage(canon_name)
                     # pulls the summary page for the species
@@ -77,6 +100,7 @@ def search_species(entry):
                     #     print(section)
                     # github push error, delete this
                     print(summary)
+                    results.setdefault(canon_name, []).append(summary)
                     # print(desc.content)
                 except:
                     print("No description found")
@@ -84,6 +108,9 @@ def search_species(entry):
                 print('GBIF Key: ' + str(key))
                 print('GBIF Species Page: ' + 'http://www.gbif.org/species/' + str(key))
                 print('Count: ' + str(occurs))
+
+                # This reads results for occurences and finds countries the species
+                # was observed in and how many occurrences there
                 occur_data = occur_search['results']
                 countries = {}
                 for occur in occur_data:
@@ -93,7 +120,7 @@ def search_species(entry):
                             countries[country] = 1
                         else:
                             countries[country] += 1
-                        # print(occur['country'])
+                            # print(occur['country'])
                     except:
                         continue
                 # for country in countries:
@@ -104,10 +131,15 @@ def search_species(entry):
                     for country in sorted_countries[:3]:
                         print(str(country))
                 print('\n')
+
+                # return canon_name, vern_name, summary
             except:
                 continue
 
+    print(results)
 
+    # To be used when calling the method
+    # return results
 
 search_species('something')
 
--- ProjectFlask.py
+++ ProjectFlask.py
@@ -4,7 +4,7 @@ from flickr import *
 from species_request import *
 from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+    abort, render_template, flash
 
 # configuration of the database
 DATABASE = './Species.db'
@@ -17,13 +17,15 @@ PASSWORD = 'default'
 
 # create the instance of the app.
 app = Flask(__name__)
-#configureations of the flask environment
+# configureations of the flask environment
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
 
 # method used to connect to the database in the location provided above in the DATABASE value
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
 
 # method that initializes the database by reading from said file
 def init_db():
@@ -31,6 +33,7 @@ def init_db():
         with app.open_resource('Speciesschema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
+
 
 # before any request can be made server needs to establish a connection with the database
 @app.before_request
@@ -49,76 +52,79 @@ def teardown_request(exception):
 # Displays the mainpage on default web page load
 @app.route('/')
 def show_entries():
-	# here is where python needs to listen for the value to come in and once it recieves that value acknowlege it and make the GBIF request
-	return render_template('Project4HomePageTemplate.html')
+    # here is where python needs to listen for the value to come in and once it recieves that value acknowlege it and make the GBIF request
+    return render_template('Project4HomePageTemplate.html')
 
 
 # This method processes input and sends it onto the database. NOTE: THERE IS NO ERROR CHECKING PRIOR TO THIS.
 # experimenting with post and get request types
 @app.route("/results", methods=['POST'])
 def search_animal_name():
+    # results of text box input from button click.
+    animalName = request.form['animalLookup']
 
-	# results of text box input from button click.
-	animalName = request.form['animalLookup']
+    # GBIF Portion
+    newAnimal = Animal
+    newAnimal.name = str(animalName)
+    data = search(animalName)
+    newAnimal.extraction(newAnimal, data)
 
-	# GBIF Portion
-	newAnimal = Animal
-	newAnimal.name = str(animalName)
-	data = search(animalName)
-	newAnimal.extraction(newAnimal,data)
-	
-	#scientificName TODO fix this to take first scientific name in list
-	#sn = scientificName[0]
-	#print("Here is the one scientific name")
-	#print(str(sn))
-	#show_descript()
-	# need to pull the first element in the scientificName array just to display something 
-	
-	#####################################
-	
-	## Flickr portion 
-	json_resp = make_flickr_query(animalName)
-	# show links for fun. 
-	show_links()
-	#have this working , but not filtered properly and isn't being stored in db yet 
-	#image1url = photoUrl_list[0]
-	
-	
-	# todo try and print links in webpage that way i know they are making it there. 
-	db = sqlite3.connect('./Species.db')
-	cur = db.cursor()
-	for row in cur.execute('Select name, sciName, desc from Species'):
-		print(row)
-	
-	
-	#return render_template('test.html',animalName=animalName)
-	return render_template('PageTwodiffBG.html')
-	#return animalName
-	
+    # scientificName TODO fix this to take first scientific name in list
+    # sn = scientificName[0]
+    # print("Here is the one scientific name")
+    # print(str(sn))
+    # show_descript()
+    # need to pull the first element in the scientificName array just to display something
 
-#TODO write this method to display page 2 with information properly formatted
+    #####################################
+
+    ## Flickr portion
+    json_resp = make_flickr_query(animalName)
+    # show links for fun.
+    show_links()
+    # have this working , but not filtered properly and isn't being stored in db yet
+    # image1url = photoUrl_list[0]
+
+
+    # todo try and print links in webpage that way i know they are making it there.
+    db = sqlite3.connect('./Species.db')
+    cur = db.cursor()
+    for row in cur.execute('SELECT name, sciName, desc FROM Species'):
+        print(row)
+
+    # return render_template('test.html',animalName=animalName)
+    return render_template('PageTwodiffBG.html')
+
+
+# return animalName
+
+
+# TODO write this method to display page 2 with information properly formatted
 @app.route("/printitems", methods=['POST'])
 def display_results():
-	# assign each value to it counterpart on the page prior to loading the page. 
-	# may need to render a new template or redirect to this one. For now i am just redirecting from home page. 
-	
-	db = sqlite3.connect('./Species.db')
-	cur = db.cursor()
-	for row in cur.execute('Select name, sciName, desc from Species'):
-		print(row)
-	return print_row()
-	
-	#cur = g.db.execute('select * from Species')
-	#entries = [dict(name=row[0], sciName=row[1], desc=row[3]) for row in cur.fetchall()] 
-	#return render_template('test.html', entries=entries)
-	
+    # assign each value to it counterpart on the page prior to loading the page.
+    # may need to render a new template or redirect to this one. For now i am just redirecting from home page.
+
+    db = sqlite3.connect('./Species.db')
+    cur = db.cursor()
+    for row in cur.execute('SELECT name, sciName, desc FROM Species'):
+        print(row)
+    return print_row()
+
+
+# cur = g.db.execute('select * from Species')
+# entries = [dict(name=row[0], sciName=row[1], desc=row[3]) for row in cur.fetchall()]
+# return render_template('test.html', entries=entries)
+
 @app.route('/printitems')
 def print_row():
-	cur = g.db.execute('Select * from Species')
-	query = 'SELECT * FROM Species'
-	db.row_factory = make_dicts
-	return redirect(url_for('display_results'))
-# how posts are added to the database using the Post method 	
+    cur = g.db.execute('Select * from Species')
+    query = 'SELECT * FROM Species'
+    db.row_factory = make_dicts
+    return redirect(url_for('display_results'))
+
+
+# how posts are added to the database using the Post method
 @app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
@@ -129,38 +135,37 @@ def add_entry():
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
-	
-#def make_dicts(cursor, row):
+
+# def make_dicts(cursor, row):
 #	return dict((cur.description[idx][0], value))
-	#for idx, value in enumerate(row)
-		
-#def query_db(query, args=(), one=False):
+# for idx, value in enumerate(row)
+
+# def query_db(query, args=(), one=False):
 #	cur = get_db().execute(query, args)
 #	rv = cur.fetchall()
 #	cur.close()
 #	return (rv[0] if rv else None) if one else rv
-	
-	
-# method that makes a simple animal table ( RUN FROM COMMAND LINE TO CREATE DB ALONG WITH INIT_DB )
-def make_db():	
 
+
+# method that makes a simple animal table ( RUN FROM COMMAND LINE TO CREATE DB ALONG WITH INIT_DB )
+def make_db():
     db = sqlite3.connect("Species.db")
     cur = db.cursor()
-	#cur.execute('drop table if exists Species')
-    cur.execute('create table Species (id INTEGER PRIMARY KEY autoincrement, Name text not null, Description text not null)')
+    # cur.execute('drop table if exists Species')
+    cur.execute(
+        'CREATE TABLE Species (id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT NOT NULL, Description TEXT NOT NULL)')
 
     #################PSUEDO CODE ############
-    #db = sqlite3.connect("flaskrDB.db")
-    #cur = db.cursor()
+    # db = sqlite3.connect("flaskrDB.db")
+    # cur = db.cursor()
 
- #cur.execute('create table species (name text, scientificName text, image1 image) ')
+    # cur.execute('create table species (name text, scientificName text, image1 image) ')
 
     db.commit()
     db.close()
     ########################################################
     # this method creates the database through python commands
 
-		
+
 if __name__ == '__main__':
     app.run()
-
